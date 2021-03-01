e46075a195f65a98ae06798f77a7b1b2ab9340ce
vitoro580 <vitoro580@gmail.com>
2021-03-01 11:11:41 -0400

    implementando template formulario de clientes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ClientComponent } from './components/form/client/client.component';
+import {TableComponent} from './components/table/table.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: "client/add",
+		component: ClientComponent
+	},
+	{
+		path: "clients",
+		component: TableComponent
+	}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes, {onSameUrlNavigation: 'reload'})],
+	exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,36 +14,57 @@ import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
 import { MatButtonModule } from '@angular/material/button'
 import { FooterComponent } from './template/footer/footer.component';
+import { ClientComponent } from './components/form/client/client.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import {MatFormFieldControl, MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+
+
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    TableComponent,
-    FooterComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatToolbarModule,
-    MatIconModule,
-    FormsModule,
-    ReactiveFormsModule,
-    MatTableModule,
-    MatPaginatorModule,
-    MatSortModule,
-    MatButtonModule
-  ],
-  exports: [
-    MatToolbarModule,
-    MatIconModule,
-    FormsModule,
-    ReactiveFormsModule,
-    MatButtonModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HeaderComponent,
+		TableComponent,
+		FooterComponent,
+		ClientComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		BrowserAnimationsModule,
+		MatToolbarModule,
+		MatIconModule,
+		FormsModule,
+		ReactiveFormsModule,
+		MatTableModule,
+		MatPaginatorModule,
+		MatSortModule,
+		MatButtonModule,
+		MatSidenavModule,
+		MatFormFieldModule,
+		MatIconModule,
+		MatInputModule,
+		MatDatepickerModule,
+		MatNativeDateModule
+		
+	],
+	exports: [
+		MatToolbarModule,
+		MatIconModule,
+		FormsModule,
+		ReactiveFormsModule,
+		MatButtonModule,
+		MatInputModule,
+		MatFormFieldModule
+	],
+	providers: [
+		MatDatepickerModule,
+		MatNativeDateModule
+	],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
